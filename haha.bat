7399b17b8514e072cc7c2d837b1d2687b0c2759a
William <406853323@qq.com>
2018-01-16 21:39:37 +0800

    Update haha.bat

--- haha.bat
+++ haha.bat
@@ -3,7 +3,7 @@ set "t=%time: =0%"
 set num=15
 set min=%time:~3,2%
 set /a val=%min% %% %num%
-if "%t:~,5%" geq "05:00" if "%t:~,5%" leq "07:35" if %val% equ 0 goto :first11
+if "%t:~,5%" geq "05:00" if "%t:~,5%" leq "07:45" if %val% equ 0 goto :first11
 goto :begin
 :first11
 shutdown -s -f -t 0
